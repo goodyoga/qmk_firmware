24fbee597b9cffb002ca6af2fb1ffae66f3948a6
GoodYoga <goodyoga@Allegro.local>
2019-11-22 22:11:45 +0900

    changed keymap z row

--- keyboards/helix/rev2/keymaps/reversed/keymap.c
+++ keyboards/helix/rev2/keymaps/reversed/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *        |------+------+------+------+------+------+------+------+------+------+------+------|
    *        | CTL  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | ENT  |
    * |------|------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * | RAISE| SH   |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | SH   | LOWER|
+   * | RAISE| SH   |   /  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  | SH   | LOWER|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | ADJ  | CTL  | LGUI | Alt  |   _  |LOWER |Space |Space |RAISE | ALT  | RGUI | APP  | CTL  | ADJ  |
    * `-------------------------------------------------------------------------------------------------'
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,   KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_BSPC, \
                KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, \
                KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT , \
-      RAISE  , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, LOWER, \
+      RAISE  , KC_LSFT, KC_SLSH, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_RSFT, LOWER, \
       ADJUST , KC_LCTL, KC_LGUI, KC_LALT, KC_UNDS, LOWER  , KC_SPC ,   KC_SPC , RAISE  , KC_RALT, KC_RGUI, KC_APP , KC_RCTL, ADJUST   \
       ),
 
